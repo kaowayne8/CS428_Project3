4e501c965c60512dee10fb861efe2e1c7ecf0bd7
Sean <thesean.kim33@gmail.com>
2021-11-24 02:37:06 -0600

    Add lamp functionality

---
+++ Assets/Scripts/Managers/ActionManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionManager : MonoBehaviour
+{
+
+    public void doPrimaryAction()
+    {
+
+        // if we are grabbing flashlight other actions should take priority
+        if (GrabbedManager.Instance.flashLightGrabbed)
+        {
+                swapFlashLightSource();
+        }
+
+    }
+
+
+    // swap the flash light on and off
+    public void swapFlashLightSource()
+    {
+        // find flash light and get its spotlight, and the front of the flash light
+        GameObject flash = GameObject.Find("HandLampModel");
+
+        GameObject light = flash.transform.GetChild(1).gameObject;
+
+        // if the light is active, turn it off and swap the material on the
+        // front of flash light
+        if (light.activeSelf)
+        {
+            light.SetActive(false);
+        }
+        // do the opposite
+        else
+        {
+            light.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/GrabbedManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// class to keep track of what items are being held in the scene
+public class GrabbedManager : MonoBehaviour
+{
+    // create a singleton
+    public static GrabbedManager Instance { get; private set; }
+
+    // booleans to track which objects are grabbed
+    public bool flashLightGrabbed = false;
+
+    // assign singleton
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // set flashLightGrabbed to true
+    public void activateFlashGrab()
+    {
+        flashLightGrabbed = true;
+    }
+
+    // set flashLightGrabbed to false
+    public void deactivateFlashGrab()
+    {
+        flashLightGrabbed = false;
+    }
+
+}
