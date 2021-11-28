abdc7818a0fa7b8d88006c57e512f11d1195123c
Sean <thesean.kim33@gmail.com>
2021-11-27 18:12:49 -0600

    Make rotation animation

--- Assets/Scripts/MenuScripts.cs
+++ Assets/Scripts/MenuScripts.cs
@@ -9,6 +9,7 @@ public class MenuScripts : MonoBehaviour
     int sceneIndex;
     [SerializeField] GameObject rotateButton;
     [SerializeField] GameObject ScenePrefab;
+    private bool rotating;
     Vector3 rotationVector;
     void Start()
     {
@@ -49,10 +50,40 @@ public class MenuScripts : MonoBehaviour
     public void rotateRoom()
     {
         Debug.Log("Rotate Room");
+
+        if (rotationVector.z != 360)
+        {
+            rotationVector.z += 90;
+        }
+        else
+        {
+            rotationVector.z = 0;
+        }
         
-        rotationVector.z += 90;
-        ScenePrefab.transform.rotation = Quaternion.Euler(rotationVector);
+        rotating = true;
+        //ScenePrefab.transform.rotation = Quaternion.Euler(rotationVector);
+        StartCoroutine(Rotate(rotationVector,5));
         Debug.Log(ScenePrefab);
         Debug.Log(ScenePrefab.transform.rotation);
+    }
+
+    private IEnumerator Rotate(Vector3 angles, float duration)
+    {
+        rotating = true;
+        Quaternion startRotation = ScenePrefab.transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(angles) * startRotation;
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            ScenePrefab.transform.rotation = Quaternion.Lerp(startRotation, endRotation, t / duration);
+            yield return null;
+        }
+        ScenePrefab.transform.rotation = endRotation;
+        rotating = false;
+    }
+
+    public void StartRotation()
+    {
+        if (!rotating)
+            StartCoroutine(Rotate(new Vector3(0, 90, 0), 1));
     }
 }
